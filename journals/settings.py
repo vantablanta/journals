d47ee056242c6ad7147ef57664a7b721712caf96
vanta blanta <vantablanta@gmail.com>
2022-08-29 07:44:13 +0300

    added aws allowed hosts

--- journals/settings.py
+++ journals/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['journals-dev.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
